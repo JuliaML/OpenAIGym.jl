09e1e313908e3ec3561277c0b90c4c9073b83b4a
Tom Breloff <tom@breloff.com>
2017-02-28 14:48:55 -0500

    add isinteractive to test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,10 @@ using Base.Test
 # write your own tests here
 @test 1 == 1
 
-
-env = GymEnv("CartPole-v0")
-for i=1:5
-    R = run_episode(()->nothing, env, RandomPolicy())
-    info("Episode $i finished. Total reward: $R")
+if isinteractive()
+    env = GymEnv("CartPole-v0")
+    for i=1:5
+        R = run_episode(()->nothing, env, RandomPolicy())
+        info("Episode $i finished. Total reward: $R")
+    end
 end
