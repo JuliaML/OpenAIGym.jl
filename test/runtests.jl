1b9733bfbdf76108424d3011fb6fd1b770f27955
Joel Mason <jobba1@hotmail.com>
2018-09-14 00:55:40 +1000

    Add tests that double as benchmarks

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,136 @@
-using OpenAIGym
-using Base.Test
+using OpenAIGym, Compat.Test
+using PyCall
 
-# write your own tests here
-@test 1 == 1
+"""
+`function time_steps(env::GymEnv{T}, num_eps::Int) where T`
 
-if isinteractive()
-    env = GymEnv("CartPole-v0")
-    for i=1:5
-        R = run_episode(()->nothing, env, RandomPolicy())
-        info("Episode $i finished. Total reward: $R")
+run through num_eps eps, recording the time taken for each step and
+how many steps were made. Doesn't time the `reset!` or the first step of each
+episode (since higher chance that it's slower/faster than the rest, and we want
+to compare the average time taken for each step as fairly as possible)
+"""
+# function time_steps(env::GymEnv{T}, num_eps::Int) where T
+function time_steps(env::GymEnv, num_eps::Int)
+    t = 0.0
+    steps = 0
+    for i in 1:num_eps
+        reset!(env)
+        step!(env, rand(env.actions)) # ignore the first step - it might be slow?
+        t += (@elapsed steps += epstep(env))
+    end
+    steps, t
+end
+
+"""
+Steps through an episode until it's `done`
+assumes env has been `reset!`
+"""
+# function epstep(env::GymEnv{T}) where T
+function epstep(env::GymEnv)
+    steps = 0
+    while !env.done
+        steps += 1
+        r, s = step!(env, rand(env.actions))
+    end
+    steps
+end
+
+@testset "Gym Basics" begin
+
+    pong = GymEnv("Pong-v4")
+    pongnf = GymEnv("PongNoFrameskip-v4")
+    pacman = GymEnv("MsPacman-v4")
+    pacmannf = GymEnv("MsPacmanNoFrameskip-v4")
+    cartpole = GymEnv("CartPole-v0")
+    bj = GymEnv("Blackjack-v0")
+
+    allenvs = [pong, pongnf, pacman, pacmannf, cartpole, bj]
+    eps2trial = Dict(pong=>4, pongnf=>4, pacman=>9, pacmannf=>9, cartpole=>5000, bj=>30000)
+    atarienvs = [pong, pongnf, pacman, pacmannf]
+    envs = allenvs
+
+    @testset "envs load" begin
+        # check they all work - no errors == no worries
+        println("------------------------------ Check envs load ------------------------------")
+        for (i, env) in enumerate(envs)
+            @show env.name env.pyenv a = rand(env.actions)|>OpenAIGym.pyaction PyObject(a)|>pytypeof
+            time_steps(env, 1)
+            @test !ispynull(env.pyenv)
+            println("------------------------------")
+        end
+    end
+
+    @testset "julia speed test" begin
+        println("------------------------------ Begin Julia Speed Check ------------------------------")
+        for env in envs
+            num_eps = eps2trial[env]
+            steps, t = time_steps(env, num_eps)
+            @show env.name num_eps t steps
+            println("microsecs/step (lower is better): ", t*1e6/steps)
+            println("------------------------------")
+        end
+        println("------------------------------ End Julia Speed Check ------------------------------\n")
+    end
+
+    @testset "python speed test" begin
+        println("------------------------------ Begin Python Speed Check ------------------------------")
+        py"""
+        import gym
+        import numpy as np
+
+        pong = gym.make("Pong-v4")
+        pongnf = gym.make("PongNoFrameskip-v4")
+        pacman = gym.make("MsPacman-v4");
+        pacmannf = gym.make("MsPacmanNoFrameskip-v4");
+        cartpole = gym.make("CartPole-v0")
+        bj = gym.make("Blackjack-v0")
+
+        allenvs = [pong, pongnf, pacman, pacmannf, cartpole, bj]
+        eps2trial = {pong: 4, pongnf: 4, pacman: 9, pacmannf: 9, cartpole: 5000, bj: 30000}
+        atarienvs = [pong, pongnf, pacman, pacmannf];
+
+        envs = allenvs
+
+        import time
+        class Timer(object):
+            elapsed = 0.0
+            def __init__(self, name=None):
+                self.name = name
+
+            def __enter__(self):
+                self.tstart = time.time()
+
+            def __exit__(self, type, value, traceback):
+                Timer.elapsed = time.time() - self.tstart
+
+        def time_steps(env, num_eps):
+            t = 0.0
+            steps = 0
+            for i in range(num_eps):
+                env.reset()
+                with Timer():
+                    steps += epstep(env)
+                t += Timer.elapsed
+            return steps, t
+
+        def epstep(env):
+            steps = 0
+            while True:
+                steps += 1
+                action = env.action_space.sample()
+                state, reward, done, info = env.step(action)
+                if done == True:
+                    break
+            return steps
+
+        for env in envs:
+            num_eps = eps2trial[env]
+            with Timer():
+                steps, s = time_steps(env, num_eps)
+            t = Timer.elapsed
+            print(f"{env} num_eps: {num_eps} t: {t} steps: {steps} \n microsecs/step (lower is better): {t*1e6/steps}")
+            print("------------------------------")
+        """
+        println("------------------------------ End Python Speed Check ------------------------------")
     end
 end
